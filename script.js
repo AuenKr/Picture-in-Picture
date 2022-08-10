89aa5caaa2a18a537600ffd929ca4f889a6e464a
Auen <auenkumar64@gmail.com>
2022-08-10 19:08:43 +0530

    Added stop/start functionallity

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const videoELement = document.getElementById('video');
 const button = document.getElementById('button');
 
 // Prompt to select media stream, pass to vedio element, then play
-async function selectMediaStream(){
+async function selectMediaStream() {
     try {
         const mediaStream = await navigator.mediaDevices.getDisplayMedia();
         videoELement.srcObject = mediaStream;
@@ -15,13 +15,23 @@ async function selectMediaStream(){
     }
 };
 
-button.addEventListener('click', async () => {
+async function startPIP() {
     // Disable Button
     button.disabled = true;
     // Start Picture in Picture
     await videoELement.requestPictureInPicture();
     // Reset button
-    button.disabled =  false;
+    button.disabled = false;
+}
+// Adding Functionallity to Button
+button.addEventListener('click', () => {
+    if (document.pictureInPictureElement) {
+        document.exitPictureInPicture()
+        button.innerText = 'START';
+    } else {
+        startPIP()
+        button.innerText = 'STOP';
+    }
 });
 
 // On start click
